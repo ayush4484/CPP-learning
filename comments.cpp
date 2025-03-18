28d8d1ff3549b4e4de43b7d39d3f35518313327f
ayush4484 <ayushraj4484@gmail.com>
2025-03-18 14:05:49 +0530

    Comments in C++

---
+++ comments.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// This is a single line comment
+
+/*
+This
+is a 
+multiline
+comment*/
+
+int main()
+{
+   int sum = 6;
+    cout<<sum;
+    return 0;
+}
