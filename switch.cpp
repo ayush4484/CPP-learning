88d7544c6f78053e2ae5d12fcd0c5d7c69fa7841
ayush4484 <ayushraj4484@gmail.com>
2025-03-22 17:11:28 +0530

    I learned switch case in c++

---
+++ switch.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter your age: "<<endl;
+    cin>>age;
+
+    switch(age)
+    {
+        case 1: 
+            cout<<"You are 1 "<<endl;
+            break;
+        case 2: 
+            cout<<"You are 2 "<<endl;
+            break;
+        case 3: 
+            cout<<"You are 3 "<<endl;
+            break;
+        case 4: 
+            cout<<"You are 4 "<<endl;
+            break;
+        case 5: 
+            cout<<"You are 5 "<<endl;
+            break;
+        case 6: 
+            cout<<"You are 6 "<<endl;
+            break;
+        case 7: 
+            cout<<"You are 7 "<<endl;
+            break;
+        case 8: 
+            cout<<"You are 8 "<<endl;
+            break;
+        case 9: 
+            cout<<"You are 9 "<<endl;
+            break;
+        case 10: 
+            cout<<"You are 10 "<<endl;
+            break;
+        case 11: 
+            cout<<"You are 11 "<<endl;
+            break;
+        case 12: 
+            cout<<"You are 12 "<<endl;
+            break;
+        case 13: 
+            cout<<"You are 13 "<<endl;
+            break;
+        case 14: 
+            cout<<"You are 14 "<<endl;
+            break;
+        case 15: 
+            cout<<"You are 15 "<<endl;
+            break;
+        case 16: 
+            cout<<"You are 16 "<<endl;
+            break;
+        case 17: 
+            cout<<"You are 17 "<<endl;
+            break;
+        case 18: 
+            cout<<"You are 18 "<<endl;
+            break;
+        default:
+        cout<<"Your age is not between 1 to 18";
+       
+    }
+    cout<<"Done with swich case";
+    return 0;
+}
